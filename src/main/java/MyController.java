29e2935ac1c5f9444561d37b343a004d791a561c
Abdullah Ali <sali219@uic.edu>
2022-11-28 12:42:46 -0600

    first commit

---
+++ src/main/java/MyController.java
@@ -0,0 +1,58 @@
+
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+
+
+
+public class MyController implements Initializable {
+    private int portNumber;
+    Server serverConnection;
+    CFourInfo info;
+
+    @FXML
+    private VBox root;
+    @FXML
+    private TextField portField;
+    @FXML
+    private ListView<String> infoList;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+
+    }
+    public void setPort(ActionEvent e) throws IOException {
+        this.portNumber = Integer.parseInt(portField.getText());
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/GameState.fxml"));
+        Parent root2 = loader.load(); //load view into parent
+        root2.getStylesheets().add("/styles/ServerStart");//set style
+        root.getScene().setRoot(root2);//update scene graph
+        createServer();
+    }
+
+    public void createServer() {
+        infoList = new ListView<>();
+        serverConnection = new Server(data -> {
+            Platform.runLater(()->{
+                infoList.getItems().add(data.toString());
+            });
+        }, portNumber);
+        infoList.getItems().add("Server created");
+    }
+}
---
+++ src/main/java/Server.java
@@ -0,0 +1,106 @@
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class Server {
+    int count = 0;
+    int portNumber;
+    ClientThread p1Thread;
+    Boolean p1Exists;
+    ClientThread p2Thread;
+    Boolean p2Exists;
+    TheServer server;
+    private Consumer<Serializable> callback;
+
+    Server(Consumer<Serializable> call, int portNumber){
+        this.portNumber = portNumber;
+        callback = call;
+        server = new TheServer();
+        server.start();
+        p1Exists = false;
+        p2Exists = false;
+    }
+
+    public class TheServer extends Thread{
+
+        public void run() {
+            try(ServerSocket mySocket = new ServerSocket(portNumber);){
+                System.out.println("Server is waiting for a client!");
+
+                while(count < 2) {
+                    ClientThread c = new ClientThread(mySocket.accept(), count);
+                    callback.accept("client has connected to server");
+
+                    if (!p1Exists) {
+                        p1Thread = c;
+                    }
+                    else {
+                        p2Thread = c;
+                    }
+
+                    c.start();
+                    count++;
+                }
+            }//end of try
+            catch(Exception e) {
+                System.out.println("didn't work");
+                callback.accept("Server socket did not launch");
+            }
+        }//end of run
+    }
+
+    class ClientThread extends Thread {
+        Socket connection;
+        int count;
+        ObjectInputStream in;
+        ObjectOutputStream out;
+
+        ClientThread(Socket s, int count) {
+            this.connection = s;
+            this.count = count;
+        }
+
+//        public void updateClients(String message) {
+//            for(int i = 0; i < clients.size(); i++) {
+//                ClientThread t = clients.get(i);
+//                try {
+//                    t.out.writeObject(message);
+//                }
+//                catch(Exception e) {}
+//            }
+//        }
+
+        public void run(){
+
+            try {
+                in = new ObjectInputStream(connection.getInputStream());
+                out = new ObjectOutputStream(connection.getOutputStream());
+                connection.setTcpNoDelay(true);
+            }
+            catch(Exception e) {
+                System.out.println("Streams not open");
+            }
+
+
+
+            while(true) {
+                try {
+                    String data = in.readObject().toString();
+                    callback.accept("client: " + count + " sent: " + data);
+
+
+                }
+                catch(Exception e) {
+                    callback.accept("OOOOPPs...Something wrong with the socket from client: " + count + "....closing down!");
+                    break;
+                }
+            }
+        }//end of run
+
+
+    }//end of client thread
+}
